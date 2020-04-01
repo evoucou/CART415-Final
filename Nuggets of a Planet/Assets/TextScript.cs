c8e8dee5696ac102a9b938ec5ec1280064d59661
evoucou <evoucou@users.noreply.github.com>
2020-03-31 21:00:26 -0400

    Dialogue Implemented

---
+++ Nuggets of a Planet/Assets/TextScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour
+{
+    public float delay = 0.1f;
+    private string fullText;
+    private string currentText = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fullText = "Hello there! It seems like you are new here. How about I show you how to play?.";
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText() {
+        for(int i = 0; i < fullText.Length; i++){
+            currentText = fullText.Substring(0,i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
